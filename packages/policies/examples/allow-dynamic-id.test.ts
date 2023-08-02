610ff894ddff883cb7bcb7b2ec78d3402d311499
chronark <dev@chronark.com>
2023-08-02 21:50:26 +0200

    test: use satisfies to make tsc happy

--- packages/policies/examples/allow-dynamic-id.test.ts
+++ packages/policies/examples/allow-dynamic-id.test.ts
@@ -1,10 +1,9 @@
-
 import { Policy } from "../src";
 import { expect, test } from "@jest/globals";
 
 type Resources = {
-    link: ["create", "read", "update", "delete"];
-    team: ["create", "read", "addMember", "delete"];
+  link: ["create", "read", "update", "delete"];
+  team: ["create", "read", "addMember", "delete"];
 };
 
 type TenantId = string;
@@ -12,25 +11,25 @@ type ResourceId = string;
 type GlobalResourceId = `${TenantId}::${keyof Resources}::${ResourceId}`;
 
 test("example", () => {
-    const newResourceId = 'xyz'
+  const newResourceId: ResourceId = "xyz";
 
-    const p = new Policy<Resources, GlobalResourceId>({
-        resources: {
-            link: {
-                "planetfall::link::*": ["create", "read"],
-                [`planetfall::link::${newResourceId}`]: ["delete"],
-            },
-        },
-    });
+  const p = new Policy<Resources, GlobalResourceId>({
+    resources: {
+      link: {
+        "planetfall::link::*": ["create", "read"],
+        [`planetfall::link::${newResourceId}` satisfies GlobalResourceId]: ["delete"],
+      },
+    },
+  });
 
-    /**
-     * Reading is allowed for all links
-     */
-    expect(p.validate(["link:read"], "planetfall::link::/abc")).toEqual({
-        valid: true,
-    });
+  /**
+   * Reading is allowed for all links
+   */
+  expect(p.validate(["link:read"], "planetfall::link::/abc")).toEqual({
+    valid: true,
+  });
 
-    expect(p.validate(["link:delete"], `planetfall::link::${newResourceId}`)).toEqual({
-        valid: true,
-    });
+  expect(p.validate(["link:delete"], `planetfall::link::${newResourceId}`)).toEqual({
+    valid: true,
+  });
 });
